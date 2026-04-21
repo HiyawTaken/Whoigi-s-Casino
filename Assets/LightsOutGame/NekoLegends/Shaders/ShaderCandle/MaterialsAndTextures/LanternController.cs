2263cedf8c878dfb688ce2eefab8c0d3a5642890
Arcturus Ash <al4776ec@gmail.com>
2026-04-21 11:20:21 -0700

    Added Baseline Lights-OutGame

---
+++ Assets/LightsOutGame/NekoLegends/Shaders/ShaderCandle/MaterialsAndTextures/LanternController.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class LanternController : MonoBehaviour 
+{
+    public bool isLit;
+    public GameObject pointLight; // Reference to the Point Light component
+    public Material litMaterial;   // Material with emission
+    public Material unlitMaterial; // Standard material
+
+    public void ToggleLantern() 
+    {
+        isLit = !isLit;
+        pointLight.SetActive(isLit);
+        GetComponent<Renderer>().material = isLit ? litMaterial : unlitMaterial;
+    }
+}
